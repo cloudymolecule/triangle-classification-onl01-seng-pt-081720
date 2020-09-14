dd99659105256ce5804bc6025b801898bc59f3aa
Dario Carlino <baires82@gmail.com>
2020-09-14 12:58:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       begin
         raise TriangleError
       end
-    elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 #|| (@side_1 + @side_3) < @side_2
+    elsif (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_1 + @side_3) <= @side_2
       begin
         raise TriangleError
       end

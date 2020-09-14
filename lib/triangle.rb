d9f44173a132956aeb8ce66d020c11f4327435cc
Dario Carlino <baires82@gmail.com>
2020-09-14 12:27:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       end
     elsif @side_1 == @side_2 && @side_2 == @side_3
       return :equilateral
-    elsif @side2 == @side3 && @side1 == @side_3 && @side1 == @side_2
+    elsif @side1 == @side2 || @side1 == @side_3 || @side2 == @side_3
       return :isosceles
     elsif @side1 != @side2 && @side1 != @side_3 && @side2 != @side_3
       return :scalene
